723d72c5a9d808c6da2aee8d81f2d3dacbbf797c
TakeiMasaki <masaki.100319@gmail.com>
2023-02-25 09:05:58 +0900

    Initial commit

---
+++ src/test/java/com/taketeru/apprecianote/ApprecianoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taketeru.apprecianote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApprecianoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
